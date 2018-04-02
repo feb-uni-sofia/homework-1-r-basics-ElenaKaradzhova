4395b7867c4cce1bcb000a3c932220f0210ee07c
ElenaKaradzhova <37775891+ElenaKaradzhova@users.noreply.github.com>
2018-04-02 13:31:43 +0300

    Add files via upload

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,29 @@
+# a)
+x <- c(4, 1, 1, 4)
+
+# b)
+y <- c(1, 4)
+
+# c)
+x - y
+
+# If the operation is performed on vectors of unequal length, R will recycle elements of the shorter vector to match the longest vector.
+
+# d)
+s <- c(x, y)
+
+# e)
+rep(s, 10)
+
+# f)
+rep(s, each = 3)
+
+# g)
+seq(7, 21, by=1)
+7:21
+
+# h)
+length(7:21)
+
+
 
--- Problem_2.R
+++ Problem_2.R
@@ -1,1 +1,57 @@
+# a) 
+xmin <- c(23.0, 20.5, 28.2, 20.3, 22.4, 17.2, 18.2)
+
+xmax <- c(25.0, 22.8, 31.2, 27.3, 28.4, 20.2, 24.1)
+
+# b)
+xmax - xmin 
+
+# c)
+avgXmin <- mean(xmin)
+avgXmin
+
+avgXmax <- mean(xmax)
+avgXmax
+
+# d)
+xmin[xmin < mean(xmin)]
+
+# e)
+xmin[xmin > mean(xmin)]
+
+# f)
+date <- c("03Mon18", "04Tue18", "05Wed18", "04Thu18", "05Fri18", "06Sat18", "07Sun18")
+date
+
+names(xmin) <- date
+names(xmax) <- date
+
+# g) 
+temperatures <- data.frame(
+  df.xmin = xmin,
+  df.xmax = xmax
+)
+
+temperatures
+# h)
+
+Fahrenheit <- xmin #F = 9/5C + 32
+temperatures[, 'df.xminFahrenheit'] <- temperatures[, 'df.xmin'] * Fahrenheit 
+
+temperatures[, 'df.xminFahrenheit']
+
+# i)
+
+measurementsInFahrenheit <- data.frame(
+  df.date = date,  
+  df.xminFahrenheit = temperatures[, 'df.xminFahrenheit']
+)
+
+measurementsInFahrenheit
+
+# j)
+
+measurementsInFahrenheit[c(1, 2, 3, 4, 5), ]
+
+
 
